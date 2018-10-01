17c7d46fc2a300d9a1f0657f4f7b337287cf8237
Alexander Suter <alexander.suter@axonivy.com>
2018-10-01 11:16:29 +0200

    Fix composer setup

---
+++ docker/apache/install-composer.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
+
+EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE="$(php -r "echo hash_file('SHA384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --version=1.4.1 --install-dir=/var
+rm composer-setup.php
+mv /var/composer.phar /usr/local/bin/composer
